f40bd1e0f5c5ffec396dcb1d0cfcaa65cc73513a
Bowie <marketing@letscode.com.br>
2023-09-04 22:02:17 -0300

    add arquivo pdf e JS

---
+++ scripts.js
@@ -0,0 +1,32 @@
+const myObserver = new IntersectionObserver((entradas) => {
+    entradas.forEach( (i) => {
+        if(i.isIntersecting){
+            i.target.classList.add('show')
+             
+        }else {
+            i.target.classList.remove('show')
+        }
+    })
+})
+
+const elements = document.querySelectorAll('.hidden') 
+
+elements.forEach((e) => myObserver.observe(e))
+
+const observ =new IntersectionObserver((entries) => {
+    entries.forEach((a) => {
+        if(a.isIntersecting) {
+            a.target.classList.add('animate')
+        }else {
+            a.target.classList.remove('animate')
+        }
+
+    })
+})
+
+
+
+
+const target =document.querySelectorAll(`[data-anime]`)
+target.forEach((e) => observ.observe(e))
+
